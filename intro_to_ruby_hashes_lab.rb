dffc09fa7a0f47a1512db1d2242946a0f17bf424
Dallas Blackwell <mdvayne923@gmail.com>
2020-05-16 03:01:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,13 +13,13 @@ def monopoly
 
 end
 
-def monopoly
+def monopoly_with_second_tier
 	monopoly = {
 	  :railroads => {:pieces=>4},:names=>{}
 	}
 end
 
-def monopoly
+def monopoly_with_third_tier
 	monopoly = {
 	  :railroads => {
 	    :pieces=>4
